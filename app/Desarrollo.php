c6d4714073d66cd0af9d58c55651c7691da311ab
jaaviergaarcia <javiergarciadv@gmail.com>
2016-05-31 01:05:15 -0500

    Jaav2.1
    Solo cree las relaciones 1:N

--- app/Desarrollo.php
+++ app/Desarrollo.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Desarrollo extends Model {
 
-	//
+	public function profesor()
+	{
+		return $this->belongsTo('App\Profesor','profesor_id');
+	}
 
 }
--- app/Patente.php
+++ app/Patente.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Patente extends Model {
 
-	//
+	public function profesor()
+	{
+		return $this->belongsTo('App\Profesor','profesor_id');
+	}
 
 }

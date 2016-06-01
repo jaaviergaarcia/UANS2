da747a7fd988ce0f0645da727d7bead3ea15e84d
Velkan Gael Manzanares <gaelgeovanimq@gmail.com>
2016-06-01 01:42:59 -0500

    velki v3
    pruebas de relaciones

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,10 +10,26 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+use App\Tutoria;
+use App\Profesor;
+use App\Libro;
 
 Route::get('admin','AdminController@admin');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('prueba_tutorias', function()
+{
+	$lastname_prof = Tutoria::find(1)->profesor;
+	return ("Holi--->".$lastname_prof);
+});
+
+Route::get('prueba_tutorias2', function()
+{
+	$tutorias_profe = Profesor::find(1)->tutorias;
+	return ("Holi--->".$tutorias_profe);
+});
+
